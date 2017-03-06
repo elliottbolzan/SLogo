3a6a08634637d22530e1f4d692aa2dbca1baf332
Jay Doherty <jld60@duke.edu>
2017-03-06 04:41:58 -0500

    Implemented turtle tracker.

--- src/view/visualization/TurtleInfo.java
+++ src/view/visualization/TurtleInfo.java
@@ -1,9 +1,27 @@
 package view.visualization;
 
+import javafx.geometry.Insets;
+import javafx.scene.layout.VBox;
+
 public class TurtleInfo {
 
-	public TurtleInfo() {
-		// TODO Auto-generated constructor stub
-	}
+	TurtleDisplay myDisplay;
+	VBox turtleInfoGraphic;
+	
+	public TurtleInfo(TurtleDisplay display, int id) {
+		myDisplay = display;
+		turtleInfoGraphic = new VBox();
+		turtleInfoGraphic.setPadding(new Insets(5, 5, 5, 5));
+		
+		SimpleDoubleLabel xLabel = new SimpleDoubleLabel("X", myDisplay.getTurtle(id).xProperty());
+		SimpleDoubleLabel yLabel = new SimpleDoubleLabel("Y", myDisplay.getTurtle(id).yProperty());
+		SimpleDoubleLabel rotateLabel = new SimpleDoubleLabel("Heading", myDisplay.getTurtle(id).rotationProperty());
+		SimpleBooleanLabel penLabel = new SimpleBooleanLabel("Pen", myDisplay.getTurtle(id).penDownProperty(), "down", "up");
 
+		turtleInfoGraphic.getChildren().addAll(xLabel.getLabel(), yLabel.getLabel(), rotateLabel.getLabel(), penLabel.getLabel());
+	}
+	
+	protected VBox getView() {
+		return turtleInfoGraphic;
+	}
 }
